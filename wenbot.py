cdc52423953c90228b2f9f72579f20e4ef068b46
WENBNB <wenbnbmeme@gmail.com>
2025-10-21 17:45:16 +0530

    Update wenbot.py

--- wenbot.py
+++ wenbot.py
@@ -187,6 +187,7 @@ def handle_buttons(update, context):
 
     # Run Flask in background so Render stays awake
     threading.Thread(target=run_flask).start()
+print("Bot connected successfully, polling started...")
 
     updater.start_polling()
     logger.info("WENBNB Cloud Bot started.")
@@ -221,6 +222,7 @@ import os
 
 # Auto-restart if Render sends stop signal
 signal.signal(signal.SIGTERM, lambda signum, frame: os.execv(sys.executable, ['python'] + sys.argv))
+
 
 
 
